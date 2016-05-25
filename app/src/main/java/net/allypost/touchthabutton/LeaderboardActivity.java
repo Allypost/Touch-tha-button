0b34806b7470e8789ca773b07456a4fc3ff62887
Josip Igrec <allypost98@gmail.com>
2016-05-26 01:35:31 +0200

    Added leaderboard rendering

--- app/src/main/java/net/allypost/touchthabutton/LeaderboardActivity.java
+++ app/src/main/java/net/allypost/touchthabutton/LeaderboardActivity.java
@@ -2,10 +2,15 @@ package net.allypost.touchthabutton;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -30,25 +35,83 @@ public class LeaderboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
+        String id = "";
+        int gamePlace = 0;
+
         double gameDuration = this.getGameDuration(savedInstanceState);
 
         if (gameDuration != 0.0) {
-            System.out.println("|>\t\tCALCULATED-POSITIONS:");
-            System.out.println("|-|>\t\t\tPOSITION:\t" + this.getTimePosition(gameDuration));
+            id = this.saveLeaderboardEntry(gameDuration);
+            gamePlace = this.getTimePosition(gameDuration) + 1;
 
             String niceDurationText = ordinal(this.getTimePosition(gameDuration) + 1);
-
-            this.saveLeaderboardEntry(gameDuration);
-
             Toast.makeText(getApplicationContext(), "You got " + niceDurationText + " place", Toast.LENGTH_LONG).show();
         }
 
-        List<Hashtable> leaderboard = this.getLeaderboard();
+        List<Hashtable> leaderboard = this.getSortedLeaderboard();
+        int numEntries = leaderboard.size() > 5 ? 5 : leaderboard.size();
 
-        System.out.println("\t\t<-->");
-        System.out.println("\t\tREGULAR-LEADERBOARD:\t" + leaderboard);
-        System.out.println("\t\t SORTED-LEADERBOARD:\t" + this.sortLeaderboard(leaderboard));
-        System.out.println("\t\t<-->");
+        LinearLayout layout = (LinearLayout) findViewById(R.id.leaderboardContainer);
+
+        for (int i = 0; i < numEntries; i++) {
+            Hashtable entry = leaderboard.get(i);
+
+            int place = (i + 1);
+
+            String entryText = getLeaderboardEntryText(entry, place);
+
+            this.renderLeaderboardEntry(layout, entryText, place == gamePlace);
+        }
+
+        if (gameDuration != 0.0 && gamePlace > 5) {
+            Hashtable entry = getDbEntry(id);
+
+            if (entry != null) {
+                String entryText = getLeaderboardEntryText(entry, gamePlace);
+
+                this.renderLeaderboardEntry(layout, entryText, true);
+            }
+        }
+    }
+
+    private void renderLeaderboardEntry(LinearLayout layout, String text, boolean special) {
+        TextView textView = new TextView(this);
+
+        textView.setText(text);
+
+        textView = styleLeaderboardEntry(textView, special);
+
+        layout.addView(textView);
+    }
+
+    private TextView styleLeaderboardEntry(TextView textView, boolean special) {
+        textView.setHeight(this.spToPx(35));
+        textView.setTextSize(16);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+
+        if (special) {
+            textView.setTypeface(Typeface.DEFAULT_BOLD);
+            textView.setTextColor(getResources().getColor(R.color.colorPrimaryDarkest));
+            textView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLighter));
+        }
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, this.spToPx(5), 0, 0);
+
+        textView.setLayoutParams(layoutParams);
+
+        return textView;
+    }
+
+    private String getLeaderboardEntryText(Hashtable entry, int gamePlace) {
+        double gameDuration = (double) entry.get("duration");
+        String date = (String) entry.get("date");
+        String gameTime = String.format(Locale.UK, "%.3f", gameDuration);
+
+        String entryText = gamePlace + ". " + date + " - " + gameTime + "s";
+
+        return entryText;
     }
 
     private double getGameDuration(Bundle savedInstanceState) {
@@ -180,18 +243,21 @@ public class LeaderboardActivity extends AppCompatActivity {
         return lines;
     }
 
-    private int getDbEntry(String id) {
+    private Hashtable getDbEntry(String id) {
         List<Hashtable> gamesList = this.getLeaderboard();
+
+        if (gamesList == null)
+            return null;
 
         for (int i = 0; i < gamesList.size(); i++) {
             Hashtable entry = gamesList.get(i);
             String entryID = entry.get("id").toString();
 
             if (id.equals(entryID))
-                return i;
+                return entry;
         }
 
-        return -1;
+        return null;
     }
 
     private int getTimePosition(double gameTime) {
@@ -242,6 +308,10 @@ public class LeaderboardActivity extends AppCompatActivity {
     public String getAndroidID() {
         return Settings.Secure.getString(this.getContentResolver(),
                 Settings.Secure.ANDROID_ID);
+    }
+
+    private int spToPx(int sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, getResources().getDisplayMetrics());
     }
 
     public static String ordinal(int i) {
